704968f8b895cb017849f4d36ca5857881d2cbca
Kriti Baru <tuo88866@temple.edu>
2024-09-01 14:55:49 -0400

    fixed linking issues between files

--- print_random.c
+++ print_random.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+#include "random.c"
 
 int main()
 {
@@ -13,7 +12,7 @@ int main()
 
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
-		putchar( randchar() );
+		putchar(randchar());
 	putchar('\n');
 
 	return(0);
